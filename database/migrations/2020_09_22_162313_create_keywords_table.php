ca863591ca34229ba644391e91807345f9bb24e8
Rosario Crisci <novagelida@gmail.com>
2020-09-22 18:31:23 +0200

    adding keyword model

---
+++ database/migrations/2020_09_22_162313_create_keywords_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeywordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keywords', function (Blueprint $table) {
+            //TODO: we might want to add a research frequency counter and, maybe, also
+            // a 'last reasearch' timestamp column
+            $table->string('value', 100);
+            $table->primary('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keywords');
+    }
+}
