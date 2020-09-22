8225ecc2bc00c359756bb3ce78d4e94e96c25c5f
Rosario Crisci <novagelida@gmail.com>
2020-09-22 18:21:44 +0200

    setting up routes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+//TODO: This route should point to something different
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/providers', 'GifProviderController@list');
+Route::get('/provider/{identifiers}/stats', 'GifProviderController@show');
+Route::post('/provider/{identifiers}', 'GifProviderController@setDefaultProvider');
+Route::get('/gifs/{keyword}', 'SearchController@search');
+Route::get('/gifs/{keyword}/stats', 'SearchController@showStatsPerKeyword');
