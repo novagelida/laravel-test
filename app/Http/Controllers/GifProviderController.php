289634f7d52ca89d45cef4985f4ed83c0226cebc
Rosario Crisci <novagelida@gmail.com>
2020-09-22 21:42:52 +0200

    properly formatting calls data; returning 404 if data not found.

--- app/Http/Controllers/GifProviderController.php
+++ app/Http/Controllers/GifProviderController.php
@@ -8,10 +8,20 @@ use Illuminate\Http\Request;
 class GifProviderController extends Controller
 {
     private const EMPTY_ARRAY_MESSAGE = "Sorry, we didn't find any available gif providers";
+    private const PROVIDER_NOT_FOUND_MESSAGE = "Sorry, we didn't find any provider with the requested identifier";
 
     public function show($identifier)
     {
-        return GifProvider::where('identifier', $identifier)->get();
+        $providerData = GifProvider::where('identifier', $identifier)->first();
+
+        if (empty($providerData->identifier))
+        {
+            abort(404, self::PROVIDER_NOT_FOUND_MESSAGE);
+        }
+
+        $toReturn = ["calls" => $providerData->calls];
+
+        return $toReturn;
     }
 
     public function list()
